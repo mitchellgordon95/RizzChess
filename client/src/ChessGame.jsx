bc0e7c28a48edaa491a2e41a550321e4350fc31c
Mitchell Gordon <mitchell.gordon95@gmail.com>
2024-11-05 22:18:32 -0500

    feat: Add game reset functionality to ChessGame component

--- client/src/ChessGame.jsx
+++ client/src/ChessGame.jsx
@@ -29,11 +29,6 @@ const ChessGame = () => {
     }
   }, [chatMessages]);
 
-  const handleGameReset = () => {
-    resetGame();
-    onClose();
-  };
-
   const handleMessageSubmit = async () => {
     if (currentMessage.trim() === '') return;
     
